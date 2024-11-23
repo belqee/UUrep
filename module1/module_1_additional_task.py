87c4104cfecaa026fe36de25ce368f2e24ef9a90
Mihail Zyryanov <sweeqty@gmail.com>
2024-11-24 01:10:37 +0500

    dictionary

---
+++ module1/module_1_additional_task.py
@@ -0,0 +1,6 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = sorted(list(students))
+averages = [sum(student_grades) / len(student_grades) for student_grades in grades]
+dictionary = dict(zip(students, averages))
+print(dictionary)
