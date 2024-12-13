008e01f7b0132d7d8467de069fe3a40132274ed4
Mihail Zyryanov <sweeqty@gmail.com>
2024-12-13 16:39:40 +0500

    fix

--- module3/module_3_5.py
+++ module3/module_3_5.py
@@ -4,9 +4,9 @@ def get_multiplied_digits(number):
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
-        return first
+        return first if first != 0 else 1
 
 result = get_multiplied_digits(40203)
 print(result)
 result2 = get_multiplied_digits(402030)
-print(result2) #По расписанному в условии алгоритму должно быть 0 а не 24, т.к. int() убирает первые нули, но нуль останется нулем?
+print(result2)
